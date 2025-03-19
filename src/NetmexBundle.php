a227edddf0d3f36fa4f71162b1e1754719acdbe1
Danny Verhoeven <d.verhoeven95@live.nl>
2025-03-19 19:05:36 +0100

    Dependency Injection

--- src/NetmexBundle.php
+++ src/NetmexBundle.php
@@ -2,6 +2,8 @@
 
 namespace Netmex\Bundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class NetmexBundle extends AbstractBundle
@@ -9,5 +11,10 @@ class NetmexBundle extends AbstractBundle
     public function getPath(): string
     {
         return __DIR__;
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
     }
 }
